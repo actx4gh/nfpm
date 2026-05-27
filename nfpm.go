e7f5bc4a1103bb00f98905b646d7246a23c5c788
Aaron Colichia <393f24ae-8efc-436e-97c8-659d1dc63618@colichia.org>
2026-05-26 21:17:53 -0500

    feat: Validate XBPS architecture overrides
    Enforce XBPS architecture validation rules using the same
    packager-specific override pattern as the existing package formats.
    
    See #718

--- nfpm.go
+++ nfpm.go
@@ -657,7 +657,7 @@ func Validate(info *Info) (err error) {
 	if info.Name == "" {
 		return ErrFieldEmpty{"name"}
 	}
-	if info.Arch == "" && (info.Deb.Arch == "" || info.RPM.Arch == "" || info.APK.Arch == "") {
+	if !hasAnyArch(info) {
 		return ErrFieldEmpty{"arch"}
 	}
 	if info.Version == "" {
@@ -678,6 +678,17 @@ func Validate(info *Info) (err error) {
 	}
 
 	return nil
+}
+
+func hasAnyArch(info *Info) bool {
+	return info.Arch != "" ||
+		info.Deb.Arch != "" ||
+		info.RPM.Arch != "" ||
+		info.APK.Arch != "" ||
+		info.ArchLinux.Arch != "" ||
+		info.IPK.Arch != "" ||
+		info.XBPS.Arch != "" ||
+		info.MSIX.Arch != ""
 }
 
 // WithDefaults set some sane defaults into the given Info.
